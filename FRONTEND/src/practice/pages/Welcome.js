0e54fb7084aa0c68722223d2b54def1469b11f60
braedonb98 <brbe3574@colorado.edu>
2022-01-13 20:23:34 -0700

    about to start contact page

--- FRONTEND/src/practice/pages/Welcome.js
+++ FRONTEND/src/practice/pages/Welcome.js
@@ -1,15 +1,14 @@
 import React  from "react";
-
-import Card from "../../shared/components/UIElements/Card"
 import WelcomeIntro from "../components/WelcomeIntro";
 import WelcomeSlideShow from "../components/WelcomeSlideShow";
+import "./Welcome.css"
 
 const Welcome = () => {
     console.log("welcome")
     return(
         <React.Fragment>
+            <WelcomeSlideShow/>
             <div className="Welcome__Body">
-                <WelcomeSlideShow/>
                 <WelcomeIntro/>
             </div>
             
